c61cbb87dc66231e3ab55e23cb75174fd8b83bcc
AmirAR-QA <aabdur-rahman@academytrainee.com>
2021-06-11 14:45:04 +0100

    asdasd

--- service2/application/tests/test_service2.py
+++ service2/application/tests/test_service2.py
@@ -9,7 +9,7 @@ class TestHome(TestCase):
 class TestQuery(TestHome):
     def test_service2(self):
         
-        response = self.client.get(url_for("encounter"))
+        response = self.client.get(url_for("home"))
 
         allencounters = ["a giant rat","a giant goat","a pack of firebreathing lizards","a very small dwarf","an evil pig summoning wizard"]
         self.assertIn(response.data.decode(), allencounters)
