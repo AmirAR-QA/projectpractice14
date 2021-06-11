9e39b546137c8617faa439db14a3784e5bd886c9
AmirAR-QA <aabdur-rahman@academytrainee.com>
2021-06-11 15:36:06 +0100

    push

--- service3/app.py
+++ service3/app.py
@@ -1,3 +1,12 @@
-from application import app
+from flask import Flask, Response
+import random
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/location', methods=['GET'])
+def location():
+    locations = ["in a forested valley","on top of a mountain","in a cave","on the outskirts of a city","while walking into a swamp"]
+    return  random.choice(locations)
 
 if __name__=="__main__": app.run(port=5002, host='0.0.0.0', debug=True)  
--- service3/application/routes.py
+++
@@ -1,9 +0,0 @@
-from application import app
-from flask import Flask, Response
-import random
-
-@app.route('/location', methods=['GET'])
-def location():
-    location = ("in a forested valley","on top of a mountain","in a cave","on the outskirts of a city","while walking into a swamp")
-    random_locations = random.choice(location)
-    return Response(random_locations)
