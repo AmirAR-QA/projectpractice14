150b21382fd99916c1c55d46a39814077ed3fcfc
AmirAR-QA <aabdur-rahman@academytrainee.com>
2021-06-13 02:44:14 +0100

    ewer

--- jenkins/deploy.sh
+++ jenkins/deploy.sh
@@ -1,10 +1,11 @@
 # #!/bin/bash
 
-# # copy over compose yaml to manager node
-# scp -i ~/.ssh/ansible_id_rsa docker-compose.yaml jenkins@manager:/home/jenkins/docker-compose.yaml
+# copy over compose yaml to manager node
+scp -i ~/.ssh/ansible_id_rsa docker-compose.yaml jenkins@ansible-manager:/home/jenkins/docker-compose.yaml
 
-# # docker stack deploy
-# ssh -i ~/.ssh/ansible_id_rsa jenkins@manager << EOF
-#     export DATABASE_URI=${DATABASE_URI}
-#     docker stack deploy --compose-file docker-compose.yaml animal_noises
-# EOF
+# docker stack deploy
+ssh -i ~/.ssh/ansible_id_rsa jenkins@ansible-manager << EOF
+    export DATABASE_URI=${DATABASE_URI}
+    export SECRET_KEY=${SECRET_KEY}
+    docker stack deploy --compose-file docker-compose.yaml animal_noises
+EOF
