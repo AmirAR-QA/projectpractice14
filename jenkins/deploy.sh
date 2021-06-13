f3b7e11172c9f4d80d0d6fe233ca648638ba5297
AmirAR-QA <aabdur-rahman@academytrainee.com>
2021-06-13 02:45:25 +0100

    erwer

--- jenkins/deploy.sh
+++ jenkins/deploy.sh
@@ -7,5 +7,5 @@ scp -i ~/.ssh/ansible_id_rsa docker-compose.yaml jenkins@ansible-manager:/home/j
 ssh -i ~/.ssh/ansible_id_rsa jenkins@ansible-manager << EOF
     export DATABASE_URI=${DATABASE_URI}
     export SECRET_KEY=${SECRET_KEY}
-    docker stack deploy --compose-file docker-compose.yaml animal_noises
+    docker stack deploy --compose-file docker-compose.yaml encounters
 EOF
